7094f92c86afb58a8542043e3857723bad954086
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-30 15:07:25 +0700

    add pagination meta data as response

--- app/service/user_service.go
+++ app/service/user_service.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"context"
+	"math"
 
+	"github.com/gofiber/fiber/v2"
 	"gitub.com/umardev500/gopos/internal/app/contract"
 	"gitub.com/umardev500/gopos/internal/app/models"
 	pkgUtil "gitub.com/umardev500/gopos/pkg/util"
@@ -19,16 +21,27 @@ func NewUserService(repo contract.UserRepository) contract.UserService {
 }
 
 func (s *userService) GetAllUsers(ctx context.Context, params *models.FindUsersParams) *pkgUtil.Response {
-	var resp = pkgUtil.Response{}
 
 	paginatedResult, err := s.repo.GetAllUsers(ctx, params)
 	if err != nil {
-		return &resp
+		return &pkgUtil.Response{}
 	}
 
-	resp.Success = true
-	resp.Message = "Find all users"
-	resp.Data = paginatedResult.Data
+	page := params.Pagination.Page
+	limit := params.Pagination.Limit
+	data := paginatedResult.Data
+	total := paginatedResult.Total
+	totalPage := math.Ceil(float64(total) / float64(limit))
 
-	return &resp
+	return &pkgUtil.Response{
+		StatusCode: fiber.StatusOK,
+		Message:    "Find all users",
+		Data:       data,
+		Pagination: &pkgUtil.PaginationMeta{
+			Page:      int64(page),
+			PageSize:  int64(limit),
+			Total:     int64(total),
+			TotalPage: int64(totalPage),
+		},
+	}
 }
