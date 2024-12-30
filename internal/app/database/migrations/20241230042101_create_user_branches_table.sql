6243309936a30e1d6f4a93ebe5f593d36df9ea09
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-30 12:50:15 +0700

    add user branch arhived table and add modifie by

--- internal/app/database/migrations/20241230042101_create_user_branches_table.sql
+++ internal/app/database/migrations/20241230042101_create_user_branches_table.sql
@@ -2,6 +2,9 @@
 CREATE TABLE IF NOT EXISTS user_branches (
     user_id UUID NOT NULL,
     branch_id UUID NOT NULL,
+    version INT NOT NULL,
+
+    modified_by VARCHAR(50) NOT NULL, -- User who made the change
 
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NULL DEFAULT NULL,
@@ -9,7 +12,27 @@ CREATE TABLE IF NOT EXISTS user_branches (
     
     PRIMARY KEY (user_id, branch_id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (branch_id) REFERENCES branches (id) ON DELETE CASCADE
+    FOREIGN KEY (branch_id) REFERENCES branches (id) ON DELETE CASCADE,
+    FOREIGN KEY (modified_by) REFERENCES users (id) ON DELETE SET NULL
+);
+
+-- Table to track changes on product units
+CREATE TABLE IF NOT EXISTS archived_user_branches (
+    user_id UUID NOT NULL,
+    branch_id UUID NOT NULL,
+    version INT NOT NULL,
+
+    modified_by VARCHAR(50) NOT NULL, -- User who made the change
+
+    archived_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ NULL DEFAULT NULL,
+    updated_at TIMESTAMPTZ NULL DEFAULT NULL,
+    deleted_at TIMESTAMPTZ NULL DEFAULT NULL,
+    
+    PRIMARY KEY (user_id, branch_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (branch_id) REFERENCES branches (id) ON DELETE CASCADE,
+    FOREIGN KEY (modified_by) REFERENCES users (id) ON DELETE SET NULL
 );
 
 -- +goose Down
