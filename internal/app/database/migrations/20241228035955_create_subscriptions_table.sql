58bb529b68d3fa834f2211e4090dda22a25300da
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-30 13:08:40 +0700

    add track row changes for subscriptions

--- internal/app/database/migrations/20241228035955_create_subscriptions_table.sql
+++ internal/app/database/migrations/20241228035955_create_subscriptions_table.sql
@@ -6,14 +6,39 @@ CREATE TABLE IF NOT EXISTS subscriptions (
     start_date DATE NOT NULL, -- Start of the subscription
     end_date DATE NOT NULL, -- End of the subscription
     is_active BOOLEAN NOT NULL DEFAULT TRUE, -- Tracks whether the subscription is active (TRUE/FALSE)
+
+    modified_by UUID NULL, -- User who made the change
     
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NULL DEFAULT NULL,
     deleted_at TIMESTAMPTZ NULL DEFAULT NULL,
     
     FOREIGN KEY (tenant_id) REFERENCES tenants(id) ON DELETE CASCADE,
-    FOREIGN KEY (plan_id) REFERENCES plans(id) ON DELETE CASCADE
+    FOREIGN KEY (plan_id) REFERENCES plans(id) ON DELETE CASCADE,
+    FOREIGN KEY (modified_by) REFERENCES users (id) ON DELETE SET NULL
+);
+
+-- Table to track changes on subscriptions
+CREATE TABLE IF NOT EXISTS archived_subscriptions (
+    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
+    tenant_id UUID NOT NULL,
+    plan_id UUID NOT NULL,
+    start_date DATE NOT NULL, -- Start of the subscription
+    end_date DATE NOT NULL, -- End of the subscription
+    is_active BOOLEAN NOT NULL DEFAULT TRUE, -- Tracks whether the subscription is active (TRUE/FALSE)
+
+    modified_by UUID NULL, -- User who made the change
+    
+    archived_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ NULL DEFAULT NULL,
+    updated_at TIMESTAMPTZ NULL DEFAULT NULL,
+    deleted_at TIMESTAMPTZ NULL DEFAULT NULL,
+    
+    FOREIGN KEY (tenant_id) REFERENCES tenants(id) ON DELETE CASCADE,
+    FOREIGN KEY (plan_id) REFERENCES plans(id) ON DELETE CASCADE,
+    FOREIGN KEY (modified_by) REFERENCES users (id) ON DELETE SET NULL
 );
 
 -- +goose Down
+DROP TABLE IF EXISTS archived_subscriptions;
 DROP TABLE IF EXISTS subscriptions;
