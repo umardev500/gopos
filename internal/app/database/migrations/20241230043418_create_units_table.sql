caf2d21c37a35848b6c495a6540079e34841bf79
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-30 11:52:56 +0700

    make tenant_id nullable in units allow shared standard units

--- internal/app/database/migrations/20241230043418_create_units_table.sql
+++ internal/app/database/migrations/20241230043418_create_units_table.sql
@@ -1,7 +1,7 @@
 -- +goose Up
 CREATE TABLE IF NOT EXISTS units (
     id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    tenant_id UUID NOT NULL,
+    tenant_id UUID NULL, -- Nullable allowing for shared units
     name VARCHAR(50) NOT NULL,
     description TEXT,
     version INT NOT NULL DEFAULT 1,
@@ -16,8 +16,8 @@ CREATE TABLE IF NOT EXISTS units (
 
 -- Table to track changes on product units
 CREATE TABLE IF NOT EXISTS archived_units (
-    id UUID PRIMARY KEY NOT NULL DEFAULT gen_random_uuid(),
-    tenant_id UUID NOT NULL,
+    id UUID PRIMARY KEY NOT NULL,
+    tenant_id UUID NULL, -- Nullable allowing for shared units
     name VARCHAR(50) NOT NULL,
     description TEXT,
     version INT NOT NULL DEFAULT 1,
