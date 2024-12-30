42e62f4c8c42aeeacfafe8f5fa9540ee37a059f7
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-30 12:59:53 +0700

    add modifiying tracking to user roles and track who made the changes

--- internal/app/database/migrations/20241230042024_create_user_roles_table.sql
+++ internal/app/database/migrations/20241230042024_create_user_roles_table.sql
@@ -2,6 +2,9 @@
 CREATE TABLE IF NOT EXISTS user_roles (
     user_id UUID NOT NULL,
     role_id UUID NOT NULL,
+    version INT NOT NULL,
+
+    modified_by VARCHAR(50) NULL, -- User who made the change
     
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NULL DEFAULT NULL,
@@ -9,8 +12,29 @@ CREATE TABLE IF NOT EXISTS user_roles (
 
     PRIMARY KEY (user_id, role_id),
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
-    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+    FOREIGN KEY (modified_by) REFERENCES users (id) ON DELETE SET NULL
+);
+
+-- Table to track changes on user roles
+CREATE TABLE IF NOT EXISTS archived_user_roles (
+    user_id UUID NOT NULL,
+    role_id UUID NOT NULL,
+    version INT NOT NULL,
+
+    modified_by VARCHAR(50) NULL, -- User who made the change
+    
+    archived_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    created_at TIMESTAMPTZ NULL DEFAULT NULL,
+    updated_at TIMESTAMPTZ NULL DEFAULT NULL,
+    deleted_at TIMESTAMPTZ NULL DEFAULT NULL,
+
+   PRIMARY KEY (user_id, role_id),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE,
+    FOREIGN KEY (role_id) REFERENCES roles (id) ON DELETE CASCADE,
+    FOREIGN KEY (modified_by) REFERENCES users (id) ON DELETE SET NULL
 );
 
 -- +goose Down
+DROP TABLE IF EXISTS archived_user_roles;
 DROP TABLE IF EXISTS user_roles;
