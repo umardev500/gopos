0381e3fc473823fb09b97365d98f4a5de9a62a47
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-31 13:15:52 +0700

    add dummy user

--- internal/app/database/migrations/20241225051110_create_users_table.sql
+++ internal/app/database/migrations/20241225051110_create_users_table.sql
@@ -17,7 +17,8 @@ CREATE TABLE IF NOT EXISTS users (
 
 -- Seed data
 INSERT INTO users (id, username, email, password_hash) VALUES
-    ('00000000-0000-0000-0000-000000000001', 'adminusername', 'admin@email.com', '$2a$10$AMLoK2T8XDVO2XAX0maDJeFpPwQfCTkzdRNceW9pp.f2saSIYP9Me');
+    ('00000000-0000-0000-0000-000000000001', 'adminusername', 'admin@email.com', '$2a$10$AMLoK2T8XDVO2XAX0maDJeFpPwQfCTkzdRNceW9pp.f2saSIYP9Me'),
+    ('00000000-0000-0000-0000-000000000002', 'superadminusername', 'superadmin@email.com', '$2a$10$AMLoK2T8XDVO2XAX0maDJeFpPwQfCTkzdRNceW9pp.f2saSIYP9Me');
 
 -- Table to track changes on users
 CREATE TABLE IF NOT EXISTS archived_users (
